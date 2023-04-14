62ef00ba6c0c5f2a4171e688b79d682ecf969e4e
Herman Schoenfeld <herman@sphere10.com>
2023-04-14 20:39:25 +1000

    Fix DRM verify issue with Auto Mouse

--- Sphere10.AutoMouse.Windows/ModuleConfiguration.cs
+++ Sphere10.AutoMouse.Windows/ModuleConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Hydrogen;
+﻿using System;
+using Hydrogen;
 using Hydrogen.Application;
 using Hydrogen.Windows;
 using Hydrogen.Windows.Forms;
@@ -23,6 +24,19 @@ namespace Sphere10.AutoMouse.Windows {
 			// Initializers/Finalizers
 			services.AddInitializer<FirstTimeSetWindowsStartupTask>();
 			//services.AddInitializer<IncrementUsageByOneInitializer>();  // done in Hydrogen.Application
+
 		}
+
+		public override void OnInitialize(IServiceProvider serviceProvider) {
+			base.OnInitialize(serviceProvider);
+			// Need to explicitly register the Hydrogen.CryptoEx so that cryptography providers are plugged into Hashers and Signers
+			Hydrogen.CryptoEx.ExplicitModuleConfiguration.Initialize();
+		}
+
+		public override void OnFinalize(IServiceProvider serviceProvider) {
+			base.OnFinalize(serviceProvider);
+			Hydrogen.CryptoEx.ExplicitModuleConfiguration.Finalize();
+		}
+
 	}
 }
