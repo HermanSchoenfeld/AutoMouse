5b4cc0fcffea51e084f11dabe47ab3600fe6a5ef
Herman Schoenfeld <herman@sphere10.com>
2026-01-05 18:38:47 +1000

    Update from commercial authorship to personal

--- Sphere10.AutoMouse.Windows/Properties/AssemblyInfo.cs
+++ Sphere10.AutoMouse.Windows/Properties/AssemblyInfo.cs
@@ -19,46 +19,46 @@ using System.Runtime.InteropServices;
 [assembly: Guid("EC221BB7-1AD9-45E6-BC3B-307C8F39CA0D")]
 
 
-[assembly: AssemblyCopyright("Copyright © Sphere 10 Software 2008 - {CurrentYear}")]
+[assembly: AssemblyCopyright("Copyright © Herman Schoenfeld 2008 - {CurrentYear}")]
 [assembly: AssemblyProductDistribution(ProductDistribution.ReleaseCandidate)]
-[assembly: AssemblyCompanyNumber("ABN 39600596316")]
+[assembly: AssemblyCompanyNumber("herman@sphere10.com")]
 [assembly: AssemblyCompanyLink("https://sphere10.com")]
 [assembly: AssemblyProductCode("2fbd6040-dece-45df-9f7a-7d2b562141ad")]
 [assembly: AssemblyProductLink("https://sphere10.com/products/automouse")]
 [assembly: AssemblyProductPurchaseLink("https://sphere10.com/products/automouse")]
 [assembly: AssemblyProductHelpCHM("{StartPath}/AutoMouse.CHM")]
 [assembly: AssemblyProductSecret("db7d8cb29fa78c6eb2d6fa268fa0ed4d06f01422adce6a17b355655c3df3f4fc")]
-#if DEBUG
-[assembly: AssemblyProductDrmApi("http://localhost:5000/api/drm")]
-#else
-[assembly: AssemblyProductDrmApi("https://services.sphere10.com/drm")]
-#endif
-[assembly: AssemblyProductLicense(
-	"""
-	{
-	  "authority": {
-	    "name": "Sphere 10 Software General Software Products",
-	    "dss": "ecdsa-secp256k1",
-	    "publicKey": "A0xL8HSZ7Cl9IYUx92/e34NPhYZHkQEaWcyU2BuJx/2T"
-	  },
-	  "license": {
-	    "Item": {
-	      "name": "Auto Mouse v1 Trial",
-	      "productKey": "0000-0000-0000-0003",
-	      "productCode": "2fbd6040-dece-45df-9f7a-7d2b562141ad",
-	      "featureLevel": "free",
-	      "expirationPolicy": "disable",
-	      "majorVersionApplicable": 1,
-	      "expirationDays": 30
-	    },
-	    "Signature": "MEQCIDhpDe8yvfMBCVWush6J5Dr/kN9qB35G5CHwJH9HDgVZAiAD/aY091s9MH2hMrh2BKOM5L3tS0hJg0lhuDCd8nVF/Q=="
-	  },
-	  "command": {
-	    "Item": {
-	      "productKey": "0000-0000-0000-0003",
-	      "action": "enable"
-	    },
-	    "Signature": "MEQCICJkeU9oVyTkIqUiZFCLMAU2oqXF8rdznuF+Ff+8jhqHAiACJPlHNZU759y1KKsYO6Yh09Iixxu5C9PsbpGcpX0+TQ=="
-	  }
-	}
-	""")]
+//#if DEBUG
+//[assembly: AssemblyProductDrmApi("http://localhost:5000/api/drm")]
+//#else
+//[assembly: AssemblyProductDrmApi("https://services.sphere10.com/drm")]
+//#endif
+//[assembly: AssemblyProductLicense(
+//	"""
+//	{
+//	  "authority": {
+//	    "name": "Sphere 10 Software General Software Products",
+//	    "dss": "ecdsa-secp256k1",
+//	    "publicKey": "A0xL8HSZ7Cl9IYUx92/e34NPhYZHkQEaWcyU2BuJx/2T"
+//	  },
+//	  "license": {
+//	    "Item": {
+//	      "name": "Auto Mouse v1 Trial",
+//	      "productKey": "0000-0000-0000-0003",
+//	      "productCode": "2fbd6040-dece-45df-9f7a-7d2b562141ad",
+//	      "featureLevel": "free",
+//	      "expirationPolicy": "disable",
+//	      "majorVersionApplicable": 1,
+//	      "expirationDays": 30
+//	    },
+//	    "Signature": "MEQCIDhpDe8yvfMBCVWush6J5Dr/kN9qB35G5CHwJH9HDgVZAiAD/aY091s9MH2hMrh2BKOM5L3tS0hJg0lhuDCd8nVF/Q=="
+//	  },
+//	  "command": {
+//	    "Item": {
+//	      "productKey": "0000-0000-0000-0003",
+//	      "action": "enable"
+//	    },
+//	    "Signature": "MEQCICJkeU9oVyTkIqUiZFCLMAU2oqXF8rdznuF+Ff+8jhqHAiACJPlHNZU759y1KKsYO6Yh09Iixxu5C9PsbpGcpX0+TQ=="
+//	  }
+//	}
+//	""")]
